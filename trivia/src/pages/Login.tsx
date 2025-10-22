8126c0d839539b0de50238ffa9e42a28253555ad
Mateja053 <161826430+Mateja053@users.noreply.github.com>
2025-10-22 18:14:40 +0200

    Create Login.tsx

---
+++ trivia/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='prose max-w-none'>
+      <h1 className='text-3xl font-bold tracking-tight'>
+        Welcome to TriviaQuest
+      </h1>
+    </section>
+  );
+}
