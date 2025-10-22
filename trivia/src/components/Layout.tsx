574a6121a944e11c0561458c5d2cdfe020f78415
Mateja053 <161826430+Mateja053@users.noreply.github.com>
2025-10-22 18:09:25 +0200

    Update Layout.tsx

--- trivia/src/components/Layout.tsx
+++ trivia/src/components/Layout.tsx
@@ -1,1 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
+export default function Layout() {
+  return (
+    <div className='min-h-screen flex flex-col bg-[#0b1f19] text-emerald-50'>
+      <Navbar />
+      <main className='flex-1'>
+        <div className='mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 py-8'>
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
