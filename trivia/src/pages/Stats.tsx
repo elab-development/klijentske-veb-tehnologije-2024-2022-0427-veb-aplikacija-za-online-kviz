449e088f62326d58ce43774b761d9c7d878aaecd
Mateja053 <161826430+Mateja053@users.noreply.github.com>
2025-10-22 18:16:54 +0200

    Create Stats.tsx

---
+++ trivia/src/pages/Stats.tsx
@@ -0,0 +1,9 @@
+export default function Stats() {
+  return (
+    <section className='prose max-w-none'>
+      <h1 className='text-3xl font-bold tracking-tight'>
+        Welcome to TriviaQuest Stats
+      </h1>
+    </section>
+  );
+}
